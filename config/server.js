b38ea41f47b28e85de1e91129f6583b2a53287b3
Bruno Yonezawa Sato <brunoyalves@gmail.com>
2018-07-25 08:01:03 -0300

    Adicionado cors

--- config/server.js
+++ config/server.js
@@ -1,13 +1,22 @@
 'use strict';
 
+// Import do módulo do Express
 const express = require('express');
 const server = express();
+
+// Import do módulo nativo do Node pra conseguir facilitar acesso ao bode das requisições
 const bodyParser = require('body-parser');
+
+//Import do módulo para que facitile a modularização da nossa aplicação 
 const consign = require('consign');
 
+//Import do módulo para configuração de cors
+const cors = require('cors')
+server.use(cors());
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
+
 consign()
     .include('./config/firebaseConfig.js')
     .then('./app/routes')
